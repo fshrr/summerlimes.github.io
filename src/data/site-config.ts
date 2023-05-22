b258308cdfb293d77a32530b48e7bd5d73d4b312
summerlimes <fahim.shahreer95@gmail.com>
2023-05-21 18:16:12 -0600

    updated hero and added more links

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -83,11 +83,19 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi there. I am Fahim. Welcome to my corner of the web!',
-        text: "I am full stack developer located in Toronto. My coding journey began back in 2015, tinkering with jQuery and WordPress to build websites for local businesses as a side gig. Since then, I've immersed myself in the exciting realms of web development, full-stack engineering, computational biology, and machine learning. From building comprehensive digital solutions for small businesses to orchestrating data intensive pipelines, my journey has been a thrilling blend of innovation and problem-solving. These days, my focus is on learning more about large-scale systems and experimenting with LLMs to build workflows and LLM-enabled applications.",
+        text: "I'm a passionate full-stack developer located in Toronto. My coding journey began back in 2015, tinkering with jQuery and WordPress to build websites for local businesses as a side gig. Since then, I've immersed myself in the exciting realms of web development, full-stack engineering, computational biology, and machine learning. From building comprehensive digital solutions for small businesses to orchestrating data-intensive pipelines, my journey has been a thrilling blend of innovation and problem-solving. I have worked with a diverse set of technologies in both Javascript and Python ecosystems, including <b>ReactJS</b>, <b>NextJS</b>, <b>NodeJS</b>, <b>FastAPI</b>, <b>PostgreSQL</b>, <b>DynamoDB</b> and <b>Langchain</b>. <br><br> These days, my focus is on learning more about large-scale systems and experimenting with LLMs to build workflows and LLM-enabled applications.",
         actions: [
             {
                 text: 'More about me',
                 href: '/about'
+            },
+            {
+                text: 'Resume',
+                href: 'https://drive.google.com/file/d/1gxcJAzPrD4m2z5Oi8SP0ZBxd2I0l_VpF/view?usp=drive_link'
+            },
+            {
+                text: 'Github',
+                href: 'https://github.com/summerlimes'
             }
         ]
     },
